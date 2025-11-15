125bf418aaeceeaab016397b6299a87342bce6e9
dragonflye0068 <maxwellsong05@gmail.com>
2025-11-14 17:48:36 -0800

    added encoder and drivetrain instance

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -8,6 +8,7 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -20,12 +21,16 @@ public class Drivetrain extends SubsystemBase {
 
   //left1 id 1
   final SparkMax leftMotor1 = new SparkMax(1, MotorType.kBrushless);
+  final RelativeEncoder leftEncoder1 = leftMotor1.getEncoder();
   //left2 id 4
   final SparkMax leftMotor2 = new SparkMax(4, MotorType.kBrushless);
+  final RelativeEncoder leftEncoder2 = leftMotor2.getEncoder();
   //right1 id 2
   final SparkMax rightMotor1 = new SparkMax(2, MotorType.kBrushless);
+  final RelativeEncoder rightEncoder1 = rightMotor1.getEncoder();
   //right2 id 3
   final SparkMax rightMotor2 = new SparkMax(3, MotorType.kBrushless);
+  final RelativeEncoder rightEncoder2 = rightMotor2.getEncoder();
 
   /** Creates a new ExampleSubsystem. */
   public Drivetrain() {
@@ -37,7 +42,7 @@ public class Drivetrain extends SubsystemBase {
     rightMotor2.configure(config, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
-  public Drivetrain getInstance() {
+  public static Drivetrain getInstance() {
     if (instance == null) instance = new Drivetrain();
     return instance;
   }
@@ -50,8 +55,8 @@ public class Drivetrain extends SubsystemBase {
   public void runMotor(double leftSpeed, double rightSpeed) {
     //0 to 12
     //clamped speed. may be changed
-    MathUtil.clamp(leftSpeed, -leftSpeed * 0.1, leftSpeed * 0.1);
-    MathUtil.clamp(rightSpeed, -rightSpeed * 0.1, rightSpeed * 0.1);
+    leftSpeed = MathUtil.clamp(leftSpeed, -0.1, 0.1);
+    rightSpeed = MathUtil.clamp(rightSpeed, -0.1, 0.1);
 
     leftMotor1.setVoltage(leftSpeed);
     leftMotor2.setVoltage(leftSpeed);
